d52bdde2b43808c55ca7cb34de030ed6953f7d1d
abid14267 <abid15-14267@diu.edu.bd>
2023-01-09 01:32:55 +0600

    added shop section

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart=props.cart;
+    // const totalPrice=cart.reduce((total,prd)=>total+prd.price,0)
+    let productPrice=0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        productPrice=productPrice+product.price;
+    }
+    let shipping=0;
+    if(productPrice>400){
+        shipping=0;
+    }
+    else if(productPrice>200){
+        shipping=4.99;
+    }
+    else if(productPrice>0){
+        shipping=12.99;
+    }
+    
+    const formatNumber = num=>{
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+
+    const tax=formatNumber(productPrice/10);
+    const groundTotal=formatNumber(productPrice+shipping+tax)
+    
+    return (
+        <div>
+            <h4>Order Summery : </h4>
+            <p>Items ordered : {cart.length}</p>
+            <p>Product Price : {formatNumber(productPrice)}</p>
+            <p><small>Shipping Cost: {shipping}</small></p>
+            <p><small>Tax + Vat :{tax}</small></p>
+            <p>Total Price : {groundTotal}</p>
+
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='headerImg'>
+                <h3>Demo E-commerce</h3>
+                <img src={logo} alt="" />
+            </div>
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Order Review</a>
+                <a href="/manage">Manage Inventory</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
